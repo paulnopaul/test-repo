b4b7cd8c0f1b6a88a0f11d7873b1d04f6c9e56a8
Павел Чеклин <p.cheklin@corp.mail.ru>
2023-04-02 20:22:45 +0300

    updated workflow

--- include/MyClass.hpp
+++ include/MyClass.hpp
@@ -2,12 +2,15 @@
 #pragma once
 
 #include <string>
+#include <cstdlib>
 
 class MyClass {
  public:
     explicit MyClass(std::string aName);
 
     std::string getName();
+
+    void func() { malloc(10); }
  private:
     std::string mName;
 };
